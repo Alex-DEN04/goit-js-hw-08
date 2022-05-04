ec0bd605f26d0c89c84ec4bc5f245997e856a736
Oleksandr Levchuk <emihrant.ua@gmail.com>
2022-05-04 20:55:18 +0200

    01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector(".gallery");
+
+const markup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", markup);
+
+function createGallery(items) {
+    return items.map((item) => {
+        const { preview, original, description } = item;
+        // console.log(item);
+        return `
+            <a class="gallery__item" 
+                href="${original}">
+            <img class="gallery__image" 
+                src="${preview}"" 
+                alt="${description}" />
+            </a>
+        `;
+    }).join(''); 
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
